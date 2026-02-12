74ca7efb9b785a74d9c627056614d9ebfa20916a
sdev <s.shakeelb4us@gmail.com>
2026-02-12 13:47:44 +0000

    update from server

---
+++ backend/src/api/logbook/controllers/logbook.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * logbook controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::logbook.logbook');
